c90eb259405d7cb044cc3e7b11149bf913f808c1
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-21 19:29:19 +0100

    feat: add action buttons

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,18 +1,35 @@
 <template>
-  <main id="main">
-    <header>
-      <div>
-        <h1>{{ t('title') }}</h1>
-        <div>
-          <router-link to="settings"> Settings </router-link>
+  <div id="main" class="min-w-full overflow-x-hidden overflow-y-scroll bg-slate-100 dark:bg-slate-800 full-height">
+    <div class="px-4 py-2 flex flex-col justify-start items-center">
+      <header class="w-full flex flex-row justify-between items-start">
+        <h1 class="text-3xl">{{ t('title') }}</h1>
+        <div class="flex flex-row flex-wrap justify-end">
+          <div class="bg-gray-300 dark:bg-gray-700 rounded-full px-4 py-1 m-2">
+            <button class="flex flex-row flex-nowrap items-center">
+              <i-ri-refresh-line class="mr-2 text-xl" />
+              <span class="text-xl">
+                {{ t('settings.title') }}
+              </span>
+            </button>
+          </div>
+          <div class="bg-gray-300 dark:bg-gray-700 rounded-full px-4 py-1 m-2">
+            <router-link to="settings" class="flex flex-row flex-nowrap items-center">
+              <i-ri-settings-5-fill class="mr-2 text-xl" />
+              <span class="text-xl">
+                {{ t('settings.title') }}
+              </span>
+            </router-link>
+          </div>
         </div>
-      </div>
-    </header>
-    <main>Cards...</main>
-  </main>
+      </header>
+      <main>Cards...</main>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
+
+const refresh = () => {};
 </script>
