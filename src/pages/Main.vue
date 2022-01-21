0aa30bffd86e549c75dc63e1d71f51b2a0bcb7e4
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-21 16:00:11 +0100

    feat: connect basic config to basic first page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,3 +1,15 @@
-<template></template>
+<template>
+  <main id="main">
+    <header>
+      <div>
+        <h1>{{ t('title') }}</h1>
+      </div>
+    </header>
+    <main>Cards...</main>
+  </main>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+</script>
