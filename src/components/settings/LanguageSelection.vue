8c113be858f7489e056e4ad4eade75106e60f82a
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-21 16:19:41 +0100

    feat: add language control

---
+++ src/components/settings/LanguageSelection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <select
+      :ref="
+        r => {
+          selection = r as HTMLSelectElement;
+        }
+      "
+      :value="selectedLng"
+      class="bg-gray-200 dark:bg-gray-700 p-1 rounded text-gray-800 dark:text-gray-200"
+    >
+      <option v-for="lng in supportedLanguages" :key="lng" :value="lng">{{ t('lng.' + lng) }}</option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { supportedLanguages } from '@/core/frameworks/i18n';
+import { useConfig } from '@/core/adapters/config';
+import { computed, onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const config = useConfig();
+const selection = ref<HTMLSelectElement>();
+const selectedLng = computed(() => config.language);
+
+onMounted(() => {
+  selection.value?.addEventListener('change', () => {
+    const lng = selection.value?.value || 'en';
+    config.setLanguage(lng);
+  });
+});
+</script>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="min-w-full overflow-x-hidden overflow-y-scroll bg-slate-100 dark:bg-slate-800 full-height">
     <div class="px-4 py-2">
-      <header class="w-full my-2">
-        <h1 class="text-5xl">Settings</h1>
+      <header class="w-full my-6">
+        <h1 class="text-5xl">{{ t('settings.title') }}</h1>
       </header>
-      <main class="my-2">...settings</main>
+      <main class="my-2 flex flex-col flex-nowrap">
+        <div class="row">
+          <p class="text-xl">{{ t('settings.language') }}</p>
+          <LanguageSelection />
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import LanguageSelection from '@/components/settings/LanguageSelection.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
+
+<style>
+.row {
+  @apply border-gray-300 dark:border-gray-600 py-3 px-4 border-t border-b w-full flex flex-row justify-between items-center;
+}
+</style>
