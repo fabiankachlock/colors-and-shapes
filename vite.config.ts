31bb2ed936f282670785470d58d8f41ccf6860ec
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-22 17:44:24 +0100

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import icons, { ViteIconsResolver } from 'vite-plugin-icons';
 import components from 'vite-plugin-components';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://gun.eco/docs/Vite
@@ -31,7 +32,37 @@ export default defineConfig({
     components({
       customComponentResolvers: ViteIconsResolver()
     }),
-    icons()
+    icons(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['/logo.png'],
+      manifest: {
+        name: 'Colors & Shapes',
+        short_name: 'Colors',
+        theme_color: '#16a34a',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1e293b',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   define: {
     'process.env.BASE_URL': process.env.VITE_BASE_URL
