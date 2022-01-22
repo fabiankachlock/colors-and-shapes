d62ecde6c0af4841bf9d2ef098372a8648f63aa0
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-22 16:04:06 +0100

    feat: add card animation

--- src/components/game/Card.vue
+++ src/components/game/Card.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="relative m-1 p-4 bg-sky-200 dark:bg-slate-700 cursor-pointer">
-    <div class="w-full h-full grid place-items-center" :class="ColorMap[card.color]">
-      <!-- Content -->
-      <div v-if="card.isOpen" :class="ShapeMap[card.shape]" class="max-h-full h-full max-w-full"></div>
-      <div v-else>
-        <p class="text-4xl">
-          {{ order }}
-        </p>
+  <div class="scene relative m-1">
+    <div class="card w-full h-full" :class="{ 'is-flipped': card.isOpen }">
+      <div class="card-face card-back p-4 bg-sky-200 dark:bg-slate-700 cursor-pointer rounded-lg">
+        <div class="w-full h-full grid place-items-center" :class="ColorMap[card.color]">
+          <div :class="ShapeMap[card.shape]" class="max-h-full h-full max-w-full"></div>
+        </div>
+      </div>
+      <div class="card-face card-front p-4 bg-sky-200 dark:bg-slate-700 cursor-pointer rounded-lg">
+        <div class="w-full h-full grid place-items-center" :class="ColorMap[card.color]">
+          <div>
+            <p class="text-4xl">
+              {{ order }}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -24,3 +31,29 @@ const props = defineProps<{
 
 const { card, order } = toRefs(props);
 </script>
+
+<style>
+.scene {
+  perspective: 600px;
+}
+
+.card {
+  transition: transform 1s;
+  transform-style: preserve-3d;
+}
+
+.card-face {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  backface-visibility: hidden;
+}
+
+.card-back {
+  transform: rotateY(180deg);
+}
+
+.card.is-flipped {
+  transform: rotateY(180deg);
+}
+</style>
