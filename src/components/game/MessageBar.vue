204b7703f571ddabb25cf08ea072db6252b82976
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-29 18:06:39 +0100

    feat: animate messages

--- src/components/game/MessageBar.vue
+++ src/components/game/MessageBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="h-full flex items-center mx-2">
-    <p class="text-xl w-full text-center h-6">
+    <p
+      class="text-xl w-full text-center h-6 transition-all transform duration-300 opacity-100"
+      :class="{ 'opacity-0 scale-90': message.length === 0 }"
+    >
       {{ message }}
     </p>
   </div>
@@ -8,7 +11,7 @@
 
 <script setup lang="ts">
 import { useMessages } from '@/core/adapters/messages';
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 
 const messages = useMessages();
 
