6b54ec2c8dad17ae042851c1bfcfd564128e774f
fabiankachlock <fabian.kachlock@icloud.com>
2022-02-03 16:51:04 +0100

    fix: color contrast & using color and shape settings

--- src/core/use-cases/colors/ColorsGameMode.ts
+++ src/core/use-cases/colors/ColorsGameMode.ts
@@ -1,3 +1,4 @@
+import { useConfig } from '@/core/adapters/config';
 import { Card } from '@/core/business/Card';
 import { GameModeConfig } from '@/core/business/GameModeConfig';
 import { Color } from './Colors';
@@ -7,8 +8,9 @@ import { Shape } from './Shapes';
 export class ColorsGameMode implements GameModeConfig<ColorCardConfig> {
   public getAllAvailableCards(): Card<ColorCardConfig>[] {
     let cards: ColorsCard[] = [];
-    for (const color of Object.values(Color)) {
-      for (const shape of Object.values(Shape)) {
+    const config = useConfig();
+    for (const color of Object.values(config.colors)) {
+      for (const shape of Object.values(config.shapes)) {
         cards.push(new ColorsCard({ color: color as Color, shape: shape as Shape }));
       }
     }
